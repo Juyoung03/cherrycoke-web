f6bfe29dd499ed641d3336a647b927deea21875f
Juyoung03 <iris031008@naver.com>
2025-07-31 11:01:37 +0900

    메인화면 챗봇 버튼 추가

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,8 +3,11 @@ import Header     from "./MainHeader";
 import SearchBox  from "../SearchBox/SearchBox";
 import SavedRoutes from "../SavedRoutes/SavedRoutes";
 import Footer      from "./Footer";
+import ChatbotImg from "../../icons/cherryBot.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function MainPage() {
+  const nav = useNavigate();
 
   return (
     <div className="min-h-screen bg-white flex flex-col pt-[88px] pb-[57px]">
@@ -12,7 +15,7 @@ export default function MainPage() {
       <Header />
 
       {/* 본문 영역 */}
-      <main className="flex-1 p-6">
+      <main className="flex-1 p-6 relative">
         {/* 페이지 타이틀 */}
         <h1
           className="
@@ -34,6 +37,16 @@ export default function MainPage() {
         {/* 저장된 경로 리스트 */}
         <div className="mt-8">
           <SavedRoutes providerId="CURRENT_USER_ID" />
+        </div>
+
+        <div>
+          <button 
+            className="absolute flex flex-col right-[16px] bottom-0"
+            onClick={() => nav("/chatbot")}
+          >
+              <img src={ChatbotImg} alt="chatbot" className="w-[65px]"/>
+              <p className="text-[12px]">체리봇</p>
+          </button>
         </div>
       </main>
 
