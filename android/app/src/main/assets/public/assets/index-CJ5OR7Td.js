5f6bf3dd2bbd4069c93a133796d9c932cf6bd252
Minji Kwon <minji@g.hongik.ac.kr>
2025-11-08 22:15:11 +0900

    add: app 소스코드 추가

---
+++ android/app/src/main/assets/public/assets/index-CJ5OR7Td.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BcNzeGot.js","./index-Bb-B-fJI.js","./index-BHZ77UJ_.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-Bb-B-fJI.js";const _=r("SplashScreen",{web:()=>t(()=>import("./web-BcNzeGot.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{_ as SplashScreen};
